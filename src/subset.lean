f93563e972d18316ba91025e08b47b4c6f3ccad6
theckl <thomas.eckl@liv.ac.uk>
2020-09-09 21:50:47 +0100

    Update subset.lean

--- src/subset.lean
+++ src/subset.lean
@@ -4,7 +4,7 @@ universes u v w
 hott_theory
 
 namespace hott
-open hott.set is_trunc
+open hott.set hott.is_trunc hott.is_equiv eq hott.trunc hott.sigma
 
 namespace subset
 
@@ -16,9 +16,8 @@ structure Subset (A : Set) :=
    (carrier : Set) (map : carrier -> A) (inj : is_set_injective map)
 
 @[hott]
-instance sset_to_set (A : Set) :
-  has_coe_to_sort (Subset A) := 
-has_coe_to_sort.mk Set (λ B, B.carrier)
+instance sset_to_set (A : Set) : has_coe_to_sort (Subset A) := 
+  has_coe_to_sort.mk Set (λ B, Subset.carrier B)
 
 attribute [instance] Subset.inj 
 
@@ -47,35 +46,98 @@ def empty_Subset (A : Set) : Subset A :=
      injective.
    * Now we have all the ingredients to construct the image as a subset of the codomain. -/  
 @[hott]
-definition image_is_set {A B : Set} (f : A -> B) : is_set (total_image f) :=
+def image_is_set {A B : Set} (f : A -> B) : is_set (total_image f) :=
     have H : forall c d : total_image f, is_prop (c = d), from 
       assume c d, 
       have forall p q : c = d, p = q, from 
         assume p q, 
         have eq1 : p..1 = q..1, from 
-          @is_prop.elim (c.1 = d.1) (is_trunc_eq -1 c.1 d.1) p..1 q..1, 
-        let B := λ (p1 : c.1 = d.1), c.2 =[p1] d.2 in    
-        have eq2 : p..2 =[eq1; B] q..2, from 
+          @is_prop.elim (c.1 = d.1) (is_trunc_eq -1 c.1 d.1) p..1 q..1,
+        let I := λ (p1 : c.1 = d.1), c.2 =[p1; λ (b : B), image f b] d.2 in    
+        have eq2 : p..2 =[eq1; I] q..2, from 
           have Hchpq : change_path eq1 p..2 = q..2, from 
             let p2 := tr_eq_of_pathover (change_path eq1 p..2),
                 q2 := tr_eq_of_pathover q..2 in
             have Heq_pq : p2 = q2, from is_prop.elim p2 q2,
-            have H_equiv : equiv (c.2 =[q..1] d.2) (q..1 ▸ c.2 = d.2), from 
-              pathover_equiv_tr_eq _ _ _,
-            let F := (equiv.to_fun H_equiv)⁻¹, fib := fiber f c.1 in
+            have H_equiv : equiv (c.2 =[q..1; λ (b : B), image f b] d.2) 
+                                 (transport (λ (b : B), image f b) q..1 c.2 = d.2), 
+              from pathover_equiv_tr_eq _ _ _,
+            let F := (equiv.to_fun H_equiv)⁻¹ᶠ, fib := fiber f c.1 in
             have HF : is_equiv F, from is_equiv_inv (equiv.to_fun H_equiv),
-            have H_trunc1 : is_prop (q..1 ▸ c.2 = d.2), from 
+            have H_trunc1 : is_prop (transport (λ (b : B), image f b) q..1 c.2 = d.2), 
+              from 
               have H_prop : is_prop (trunc -1 fib), from is_trunc_trunc -1 _ ,
               have H_set : is_set (trunc -1 fib), from is_trunc_succ _ _,
-              !is_trunc_eq,
-            have H_trunc2 : is_prop (c.2 =[q..1] d.2), from is_trunc_is_equiv_closed -1 F,
+              is_trunc_eq -1 _ _,
+            have H_trunc2 : is_prop (c.2 =[q..1; λ (b : B), image f b] d.2), from 
+              is_trunc_is_equiv_closed -1 F H_trunc1,
             is_prop.elim (change_path eq1 p..2) q..2,
           pathover_of_change_path eq1 p..2 q..2 Hchpq,
         sigma_eq2 eq1 eq2,
       is_prop.mk this,
-    is_trunc_succ_intro (total_image f) -1
+    is_trunc_succ_intro H
 
-#check coe_sort
+@[hott]
+def image_Set {A B : Set} (f : A -> B) := 
+  Set.mk (total_image f) (image_is_set f)
+
+@[hott]
+def image_embedding {A B : Set} (f : A -> B) : (image_Set f) -> B :=
+  λ (bf : total_image f), bf.1
+
+@[hott]
+lemma im_emb_is_inj {A B : Set} (f : A -> B) : 
+  is_set_injective (image_embedding f) :=
+let im_emb := image_embedding f in
+show forall (bf1 bf2 : total_image f), im_emb bf1 = im_emb bf2 -> bf1 = bf2, from
+assume bf1 bf2,
+match bf1, bf2 with (sigma.mk b1 f1), (sigma.mk b2 f2) :=
+  assume (im_emb_eq : b1 = b2), 
+  have tr_eq : im_emb_eq ▸ f1 = f2, from is_prop.elim _ _,
+  have fib_eq : f1 =[im_emb_eq; λ (b : B), image f b] f2, from 
+    pathover_of_tr_eq tr_eq,
+  apd011 sigma.mk im_emb_eq fib_eq 
+end  
+
+@[hott]
+def Image {A B : Set} (f : A -> B) : Subset B :=
+  Subset.mk (image_Set f) (image_embedding f) (im_emb_is_inj f)
+
+@[hott]
+lemma ap_car_ap_sset_mk {A : Set} (carB : Set) (mapB : carB -> A) 
+  {inj1 inj2 : is_set_injective mapB} : forall (inj_eq : inj1 = inj2), 
+  ap Subset.carrier (ap (Subset.mk carB mapB) inj_eq) = idpath carB :=
+let car_sset := λ (inj1 inj2 : is_set_injective mapB) (inj_eq : inj1 = inj2),
+                  ap Subset.carrier (ap (Subset.mk carB mapB) inj_eq) = idpath carB in
+have H : Π (inj1 : is_set_injective mapB), car_sset inj1 inj1 idp, from
+  assume inj1, 
+  show ap Subset.carrier (ap (Subset.mk carB mapB) (idpath inj1)) = idpath carB,
+    by reflexivity,
+assume inj_eq, rec_unbased H inj_eq
+
+/- The type of subsets of [A] is a set: the power set. 
+   To show this we first characterize equalities of subsets : they correspond to
+   bijections of the underlying sets compatible with the inclusion maps.
+   The core of the construction of this equivalence is that there may be many bijections 
+   between the underlying sets, but the compatibility makes the bijection unique. -/
+@[hott, reducible]
+def sset_bijection {A : Set} (B C : Subset A) :=
+  Σ (f : bijection ↥B ↥C), (Subset.map C) ∘ (bijection.map f) = (Subset.map B) 
+
+@[hott, reducible]
+def sset_identity {A : Set} : Π (B : Subset A), sset_bijection B B 
+| (Subset.mk carB mapB injB) :=
+  have bij_comp : mapB ∘ (bijection.map (identity carB)) = mapB, from 
+    calc mapB ∘ (bijection.map (identity carB)) = mapB ∘ (id_map carB) : 
+         by rwr identity_to_id_map carB
+         ... = mapB : id_map_is_right_neutral mapB,
+  sigma.mk (identity carB) bij_comp 
+
+@[hott]
+lemma sset_ident_to_ident {A : Set} : Π (B : Subset A),
+  (sset_identity B).1 = identity (Subset.carrier B)
+| (Subset.mk carB mapB injB) :=
+  by reflexivity
 
 end subset
 
