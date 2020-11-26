e97fb1c71074d14dfa40e3d021560df4a95e4de4
theckl <thomas.eckl@liv.ac.uk>
2020-11-26 22:47:08 +0000

    Update prop_logic.lean

--- src/prop_logic.lean
+++ src/prop_logic.lean
@@ -9,21 +9,22 @@ set_option pp.implicit true
 namespace hott
 open is_trunc trunc equiv is_equiv
 
+/- We define [True] and [False] as (bundled) propositions. -/
 @[hott]
 inductive true : Type u
 | intro : true
 
 @[hott]
-lemma eq_true : forall t₁ t₂ : true, t₁ = t₂ :=
+def eq_true : forall t₁ t₂ : true, t₁ = t₂ :=
 begin 
   intros, 
   induction t₁, 
   induction t₂,
-  exact (refl true.intro)
+  exact (refl true.intro),
 end 
 
-@[hott]
-lemma is_prop_true : is_prop true :=
+@[hott, instance]
+def is_prop_true : is_prop true :=
   is_prop.mk eq_true 
 
 @[hott]
@@ -32,5 +33,20 @@ def True : Prop :=
 
 @[hott]
 inductive false : Type u
+
+@[hott]
+def eq_false : forall f₁ f₂ : false, f₁ = f₂ :=
+begin
+  intros,
+  induction f₁,
+end  
+
+@[hott, instance]
+def is_prop_false : is_prop false :=
+  is_prop.mk eq_false
+
+@[hott]
+def False : Prop :=
+  Prop.mk false is_prop_false
 
 end hott
