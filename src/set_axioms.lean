3491b14bede71f11f1d1dfefe90612a15e55effd
theckl <thomas.eckl@liv.ac.uk>
2020-12-04 22:42:08 +0000

    Update set_axioms.lean

--- src/set_axioms.lean
+++ src/set_axioms.lean
@@ -15,10 +15,18 @@ inductive Two : Type _
 | zero : Two 
 | one : Two 
 
+@[hott]
+def Two_is_set : is_set Two :=
+  sorry
+
+@[hott]
+def Two_Set : Set :=
+  Set.mk Two Two_is_set   
+
 /- Looks like a HoTT-ism but is a way to construct sets - which can be interpreted
    as subsets. Therefore it is also contained as [is_set_pred] in [subset.lean]. 
    But we want to avoid dependencies.  -/
-@[hott]   
+@[hott, instance]   
 def Sigma_Set_Prop_is_set (X : Set.{u}) (P : X -> trunctype.{u} -1) : 
   is_set (Σ x : X, P x) := 
 have forall (x : X), is_set (P x), from 
@@ -27,27 +35,34 @@ have forall (x : X), is_set (P x), from
   is_trunc_succ (P x) -1, 
 is_trunc_sigma (λ x : X, ↥(P x)) 0    
 
+@[hott, instance]
+def fib_set_map_is_set {A B : Set.{u}} (f : A -> B) (b : B) : is_set (fiber f b) :=
+  have sig_fib_is_set : is_set (Σ a : A, f a = b), from 
+    let P := λ a : A, Prop.mk (f a = b) (is_prop.mk (@is_set.elim _ _ (f a) b)) in
+    Sigma_Set_Prop_is_set A P,
+  is_trunc_equiv_closed_rev 0 (fiber.sigma_char f b) sig_fib_is_set 
+
 /- The two versions of the Axiom of Choice, as presented in the HoTT-Book, (3.8.1)
    and (3.8.3). We postulate both as axioms, to keep track of their use, even if by 
    Lem.3.8.2, the two are equivalent. -/
 @[hott]
-def AxChoice := Π (X : Set.{u}) (A : X -> Set.{u}) 
+def Choice := Π (X : Set.{u}) (A : X -> Set.{u}) 
   (P: Π x : X, A x -> trunctype.{u} -1),
 (forall x : X, ∥ Σ a : A x, P x a ∥) -> ∥ Σ (g : Π x : X, A x), Π x : X, P x (g x) ∥   
 
 @[hott]
-axiom AC : AxChoice
+axiom AC : Choice
 
 @[hott]
-def AxChoice_nonempty := Π (X : Set.{u}) (Y : X -> Set.{u}), 
+def Choice_nonempty := Π (X : Set.{u}) (Y : X -> Set.{u}), 
   (Π x : X, ∥ Y x ∥) -> ∥ Π x : X, Y x ∥ 
 
 @[hott]
-axiom AC_nonempty : AxChoice_nonempty
+axiom AC_nonempty : Choice_nonempty
 
 @[hott]
-lemma AxChoice_equiv : AxChoice.{u} ↔ AxChoice_nonempty.{u} :=
-  have imp1 : AxChoice.{u} -> AxChoice_nonempty.{u}, from 
+lemma AxChoice_equiv : Choice.{u} ↔ Choice_nonempty.{u} :=
+  have imp1 : Choice.{u} -> Choice_nonempty.{u}, from 
     assume AxCh, assume X Y pi_trunc, 
     have pi_trunc_T : Π x : X, ∥ Σ y : Y x, True ∥, from 
       assume x, trunc_functor -1 (λ y : Y x, ⟨y,true.intro⟩) (pi_trunc x),
@@ -56,7 +71,7 @@ lemma AxChoice_equiv : AxChoice.{u} ↔ AxChoice_nonempty.{u} :=
     have sigma_pi_T : (Σ (g : Π x : X, Y x), ∀ x : X, True) -> (Π x : X, Y x), from
       assume sigma_T, sigma_T.1,    
     trunc_functor -1 sigma_pi_T trunc_sigma_T,
-  have imp2 : AxChoice_nonempty -> AxChoice, from 
+  have imp2 : Choice_nonempty -> Choice, from 
     assume AxCh_ne, assume X A P pi_trunc, 
     have sigma_P_is_set : Π x : X, is_set (Σ (a : A x), P x a), from 
       assume x, Sigma_Set_Prop_is_set (A x) (P x),
@@ -69,11 +84,10 @@ lemma AxChoice_equiv : AxChoice.{u} ↔ AxChoice_nonempty.{u} :=
   (imp1, imp2)
 
 /- The Law of the Excluded Middle, following the HoTT-book, (3.4.1) -/
-@[hott]
-def LawExcludedMiddle := Π (A : Type u) [is_prop A], A ⊎ ¬ A
+def ExcludedMiddle := Π (A : trunctype.{u} -1), A ⊎ ¬ A
 
 @[hott]
-axiom LEM : LawExcludedMiddle
+axiom LEM : ExcludedMiddle
 
 /- We follow the proof of Diaconescu (HoTT-book, Thm.10.1.14) to show that the Axiom 
    of Choice (in its second version [AxChoice_nonempty]) implies the Law of the Excluded 
@@ -86,9 +100,20 @@ def susp_of_prop_is_set (A : Type u) [is_prop A] : is_set (⅀ A) :=
   sorry
 
 @[hott] 
-def AC_implies_LEM : AxChoice_nonempty -> LawExcludedMiddle :=
-  assume AC, assume A h, 
-  have f : Two -> ⅀ A, from assume pt, Two.rec north south pt,
+def AC_implies_LEM : Choice_nonempty.{u} -> ExcludedMiddle.{u} :=
+  assume AC, assume A, 
+  let f := λ pt : Two.{u}, @Two.rec (λ t : Two, ⅀ A) (@north A) (@south A) pt in 
+  have zn : f Two.zero = north, by refl,
+  have zs : f Two.one = south, by refl,
+  have f_is_surj : @is_surjective Two (⅀ A) f, from 
+    have PN : @image Two (⅀ A) f north, from @image.mk Two (⅀ A) f _ Two.zero zn,
+    have PS : @image Two (⅀ A) f south, from @image.mk Two (⅀ A) f _ Two.one zs,
+    have Pm : Π a : A, PN =[merid a; λ x : susp A, @image Two (⅀ A) f x] PS, from 
+      assume a, pathover_prop_eq _ (merid a) PN PS,
+    assume x, susp.rec PN PS Pm x,
+  let X := Set.mk (⅀ A) (susp_of_prop_is_set A),
+      Y := λ x : X, Set.mk (@fiber Two (⅀ A) f x) (@fib_set_map_is_set Two_Set X f x) in 
+  have mere_g : ∥ Π x : X, @fiber Two (⅀ A) f x ∥, from AC X Y f_is_surj,  
   sorry
 
 end hott
