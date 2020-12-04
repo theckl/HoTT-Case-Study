1be14371cbcb1a225350d163b712fca7bf2adaf2
theckl <thomas.eckl@liv.ac.uk>
2020-12-04 22:42:26 +0000

    Update prop_logic.lean

--- src/prop_logic.lean
+++ src/prop_logic.lean
@@ -49,4 +49,10 @@ def is_prop_false : is_prop false :=
 def False : Prop :=
   Prop.mk false is_prop_false
 
+/- Inhabited [Prop]s over equalities have pathover. -/
+@[hott]
+def pathover_prop_eq {A : Type.{u}} (P : A -> trunctype.{u} -1) {a₁ a₂ : A} (e : a₁ = a₂) :
+  ∀ (p₁ : P a₁) (p₂ : P a₂), p₁ =[e; λ a : A, P a] p₂ :=
+assume p₁ p₂, concato_eq (pathover_tr e p₁) (is_prop.elim (e ▸ p₁) p₂)   
+
 end hott
