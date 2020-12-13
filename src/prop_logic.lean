3445a6d200a7fe348481d331c4bd9fa6f923dea4
theckl <thomas.eckl@liv.ac.uk>
2020-12-13 23:02:17 +0000

    Update prop_logic.lean

--- src/prop_logic.lean
+++ src/prop_logic.lean
@@ -7,7 +7,7 @@ set_option pp.universes true
 set_option pp.implicit true
 
 namespace hott
-open is_trunc trunc equiv is_equiv
+open is_trunc trunc equiv hott.is_equiv
 
 /- We define [True] and [False] as (bundled) propositions. -/
 @[hott]
@@ -54,5 +54,15 @@ def False : Prop :=
 def pathover_prop_eq {A : Type.{u}} (P : A -> trunctype.{u} -1) {a₁ a₂ : A} (e : a₁ = a₂) :
   ∀ (p₁ : P a₁) (p₂ : P a₂), p₁ =[e; λ a : A, P a] p₂ :=
 assume p₁ p₂, concato_eq (pathover_tr e p₁) (is_prop.elim (e ▸ p₁) p₂)   
+
+/- Logically equivalent mere propositions are equivalent. -/
+@[hott]
+def is_prop_iff_equiv : 
+  Π {A B : Type.{u}} [is_prop A] [is_prop B], (A ↔ B) -> (A ≃ B) :=
+assume A B pA pB AiffB,
+let AB := AiffB.1, BA := AiffB.2 in
+have rinv : Π b : B, AB (BA b) = b, from assume b, @is_prop.elim B pB _ _,
+have linv : Π a : A, BA (AB a) = a, from assume a, @is_prop.elim A pA _ _,
+equiv.mk AB (adjointify AB BA rinv linv)
 
 end hott
