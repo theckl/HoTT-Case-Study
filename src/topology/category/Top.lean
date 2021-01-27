a8810d22d488abefe9654b039d5f9d6d5d9336b6
theckl <thomas.eckl@liv.ac.uk>
2021-01-27 22:44:58 +0000

    Create Top.lean

---
+++ src/topology/category/Top.lean
@@ -0,0 +1,23 @@
+import topology.basic 
+
+universe u
+hott_theory
+
+namespace hott
+open hott.set hott.subset 
+
+/- The category of topological spaces and continuous maps. -/
+@[hott]
+structure Top :=
+(carrier : Set)
+(top_str : topological_space carrier)
+
+@[hott]
+instance Top_to_Set : has_coe_to_sort Top.{u} :=
+  has_coe_to_sort.mk Set.{u} (λ T : Top, T.carrier)
+
+@[hott]
+instance topological_space_unbundled (X : Top) : 
+  topological_space ↥X := X.top_str
+
+end hott
