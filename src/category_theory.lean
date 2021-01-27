84e82b2b7450079e9a7b4e9e526b153278c9a639
theckl <thomas.eckl@liv.ac.uk>
2021-01-27 22:44:46 +0000

    Create category_theory.lean

---
+++ src/category_theory.lean
@@ -0,0 +1,35 @@
+import setalgebra
+
+universes v u w
+hott_theory
+
+namespace hott
+open hott.set hott.subset 
+
+/-
+We introduce precategories and categories following the HoTT book, 
+Sec. 9.1. HoTT precategories have sets of homomorphisms, and HoTT categories 
+prescribe univalence : Isomorphisms are equivalent to identities of objects.
+
+As far as possble we copy the mathlib-code in [category_theory.category.default].
+-/
+
+/-- A 'notation typeclass' on the way to defining a precategory. -/
+@[hott]
+class has_hom (obj : Type u) : Type (max u (v+1)) :=
+(hom : obj → obj → Set.{v})
+
+infixr ` ⟶ `:10 := has_hom.hom  -- type as \h
+
+/-- A preliminary structure on the way to defining a precategory,
+containing the data, but none of the axioms. -/
+@[hott]
+class category_struct (obj : Type u)
+extends has_hom.{v} obj : Type (max u (v+1)) :=
+(id       : Π X : obj, hom X X)
+(comp     : Π {X Y Z : obj}, (X ⟶ Y) → (Y ⟶ Z) → (X ⟶ Z))
+
+notation `𝟙` := category_struct.id -- type as \b1
+infixr ` ≫ `:80 := category_struct.comp -- type as \gg
+
+end hott
