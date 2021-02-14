10e7cc39fa5f18c27d09df7f1e3dbed55f60dbf0
theckl <thomas.eckl@liv.ac.uk>
2021-02-14 23:11:44 +0000

    Update Top.lean

--- src/topology/category/Top.lean
+++ src/topology/category/Top.lean
@@ -20,4 +20,16 @@ instance Top_to_Set : has_coe_to_sort Top.{u} :=
 instance topological_space_unbundled (X : Top) : 
   topological_space ↥X := X.top_str
 
+namespace topology
+
+variables (X : Top.{u})
+
+/- The precategory of open sets in a topological space. 
+   Morphisms are inclusions, and we derive the functorial properties of these morphisms 
+   from the weak (or partial) order on subsets. -/
+@[hott]
+def open_sets := {U ∈ (𝒫 ↥X) | prop_lift (X.top_str.is_open U)}
+
+end topology
+
 end hott
