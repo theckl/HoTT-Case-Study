ced6a55ea4292eb2d5822cf2d25d2dd4a3e045b6
theckl <thomas.eckl@liv.ac.uk>
2021-02-15 23:27:52 +0000

    Update Top.lean

--- src/topology/category/Top.lean
+++ src/topology/category/Top.lean
@@ -1,10 +1,10 @@
-import topology.basic 
+import topology.basic category_theory
 
 universe u
 hott_theory
 
 namespace hott
-open hott.set hott.subset 
+open hott.set hott.subset category_theory
 
 /- The category of topological spaces and continuous maps. -/
 @[hott]
@@ -26,9 +26,12 @@ variables (X : Top.{u})
 
 /- The precategory of open sets in a topological space. 
    Morphisms are inclusions, and we derive the functorial properties of these morphisms 
-   from the weak (or partial) order on subsets. -/
+   from the weak (or partial) order on subsets induced by inclusions. -/
 @[hott]
 def open_sets := {U ∈ (𝒫 ↥X) | prop_lift (X.top_str.is_open U)}
+
+@[hott, instance]
+def open_set_has_hom : has_hom (↥open_sets X) := sorry
 
 end topology
 
