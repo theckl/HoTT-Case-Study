0ead5048cb5bbbcfbe4f692b3703cbbee3d0bfff
theckl <thomas.eckl@liv.ac.uk>
2021-02-25 22:47:06 +0000

    Update categorial_limits.lean

--- src/categorial_limits.lean
+++ src/categorial_limits.lean
@@ -4,7 +4,7 @@ universes v u v' u' w
 hott_theory
 
 namespace hott
-open hott.set hott.subset hott.category_theory
+open hott.trunc hott.set hott.subset hott.category_theory
 
 /- We introduce limits of diagrams mapped to categories, by using cones to 
    pick the universal object and encode the universal property.
@@ -23,8 +23,30 @@ structure is_limit {J : Set.{v}} [precategory J] {C : Type u} [precategory C]
   {F : J ⥤ C} (t : cone F) :=
 (lift : Π (s : cone F), s.X ⟶ t.X)
 (fac  : ∀ (s : cone F) (j : J), lift s ≫ t.π.app j = s.π.app j)
-(uniq : ∀ (s : cone F) (m : s.X ⟶ t.X) (w : ∀ j : J, m ≫ t.π.app j = s.π.app j),
-           m = lift s)
+(uniq : ∀ (s : cone F) (m : s.X ⟶ t.X) 
+          (w : ∀ j : J, m ≫ t.π.app j = s.π.app j), m = lift s)
+
+/- `limit_cone F` contains a cone over `F` together with the information that 
+   it is a limit. -/
+@[hott]
+structure limit_cone {J : Set.{v}} [precategory J] {C : Type u} [precategory C] 
+  (F : J ⥤ C) :=
+(cone : cone F)
+(is_limit : is_limit cone)
+
+/- `has_limit F` represents the mere existence of a limit for `F`. This allows
+   to define as a class with instances. However, if `C` is a category, 
+   `limit_cone F` is a (-1)-Type since limits are determined up to isomorphism,
+   so we can extract a limit cone from `has_limit`. -/
+@[hott]   
+class has_limit {J : Set.{v}} [precategory J] {C : Type u} [precategory C] 
+  (F : J ⥤ C) :=
+mk' :: (exists_limit : ∥limit_cone F∥)
+
+@[hott]
+def has_limit.mk {J : Set.{v}} [precategory J] {C : Type u} [precategory C] 
+  {F : J ⥤ C} (d : limit_cone F) :=
+has_limit.mk' (tr d)  
 
 end category_theory.limits
 
