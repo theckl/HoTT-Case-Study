a7c896b154463bc84491a0bccda2c1726ba07bf9
theckl <thomas.eckl@liv.ac.uk>
2021-03-03 23:04:49 +0000

    Update pathover2.lean

--- src/pathover2.lean
+++ src/pathover2.lean
@@ -5,6 +5,7 @@ hott_theory
 
 namespace hott
 
+/- All these equalities should be produced by tactics. -/
 @[hott]
 def ap0111 {A : Type u} {B : Type v} {C D : A -> B -> Type _} {E : Type _}
   (f : Π (a : A) (b : B), C a b -> D a b -> E)
@@ -13,5 +14,19 @@ def ap0111 {A : Type u} {B : Type v} {C D : A -> B -> Type _} {E : Type _}
   (p₄ : d₁ =[ap011 D p₁ p₂; hott.set.id] d₂) :
   f a₁ b₁ c₁ d₁ = f a₂ b₂ c₂ d₂ := 
 begin hinduction p₁, hinduction p₂, hinduction p₃, hinduction p₄, refl end  
+
+@[hott]
+def apdo0111 {A : Type _} {B C : A -> Type _} {D : Π a : A, B a -> Type _}
+  (f : Π (a : A) (b : B a) (d : D a b), C a)
+  {a₁ a₂ : A} {b₁ : B a₁} {b₂ : B a₂} {d₁ : D a₁ b₁} {d₂ : D a₂ b₂}
+  (p : a₁ = a₂) (q : b₁ =[p] b₂) (r : d₁ =[apd011 D p q; hott.set.id] d₂) :
+  f a₁ b₁ d₁ =[p] f a₂ b₂ d₂ :=
+begin hinduction p, hinduction q, hinduction r, refl end 
+
+@[hott]
+def tr_fn_tr_eval {A B : Type _} {C : A -> B -> Type _} {a₁ a₂ : A}
+  (f : Π (b : B), C a₁ b) (p : a₁ = a₂) (b : B) :
+  (p ▸ f) b = p ▸ (f b) :=
+begin hinduction p, refl end  
 
 end hott
