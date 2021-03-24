87418e01ad63b8b05d9a1c98ffcc99ea6b6e12b7
theckl <thomas.eckl@liv.ac.uk>
2021-03-24 23:14:04 +0000

    Update Top_sheaves.lean

--- src/topology/category/Top_sheaves.lean
+++ src/topology/category/Top_sheaves.lean
@@ -5,6 +5,7 @@ hott_theory
 
 namespace hott
 open hott.set hott.subset hott.category_theory category_theory.limits
+  category_theory.opposite
 
 /- The category of topological spaces and continuous maps. -/
 @[hott]
@@ -61,55 +62,56 @@ def presheaf (C : Type u) [category.{v} C] := (↥(open_sets X))ᵒᵖ ⥤ C
 
 /- The product of the sections of a presheaf over a family of open sets. -/
 @[hott]
-def pi_opens {C : Type u} [category.{v} C] [hp : has_products C]
-  {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : C :=
-have hls : has_limits_of_shape (discrete I) C, from hp I,    
-∏ (λ i : I, F.obj (opposite.op (U i)))
+def pi_opens {C : Type u} [category.{v} C] [has_products C]
+  {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : C :=   
+have hls : has_limits_of_shape (discrete I) C, from 
+  has_products.has_limit_of_shape C I, 
+∏ (λ i : I, F.obj (op (U i)))
 
 /- The product of the sections of a presheaf over the pairwise intersections 
    of a family of open sets.-/
 @[hott]
-def pi_inters {C : Type u} [category.{v} C] [hp : has_products C]
+def pi_inters {C : Type u} [category.{v} C] [has_products C]
   {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : C :=
-have hls : has_limits_of_shape (discrete (I × I)) C, from hp (I × I),  
-∏ (λ p : ↥(I × I), F.obj (opposite.op (U p.1 ∩ U p.2)))
+have hls : has_limits_of_shape (discrete (I × I)) C, from 
+  has_products.has_limit_of_shape C (I × I),  
+∏ (λ p : ↥(I × I), F.obj (op (U p.1 ∩ U p.2)))
 
 @[hott]
-def left_res {C : Type u} [category.{v} C] [hp : has_products C]
+def left_res {C : Type u} [category.{v} C] [has_products C]
   {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : 
-  (@pi_opens X _ _ hp _ U F) ⟶ (@pi_inters X _ _ hp _ U F) :=
+  (pi_opens X U F) ⟶ (pi_inters X U F) :=
 pi.lift C (λ p : ↥I × I, pi.π _ p.1 ≫ 
                             F.map (hom_op (inter_sset_l (U p.1) (U p.2))))
 
 @[hott]
-def right_res {C : Type u} [category.{v} C] [hp : has_products C]
+def right_res {C : Type u} [category.{v} C] [has_products C]
   {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : 
-  (@pi_opens X C _ hp _ U F) ⟶ (@pi_inters X C _ hp _ U F) :=
+  (pi_opens X U F) ⟶ (pi_inters X U F) :=
 pi.lift C (λ p : ↥I × I, pi.π _ p.2 ≫ 
                             F.map (hom_op (inter_sset_r (U p.1) (U p.2))))
 
-set_option trace.class_instances true
-
 @[hott]
-def res {C : Type u} [category.{v} C] [hp : has_products C]
+def res {C : Type u} [category.{v} C] [has_products C]
   {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) :
-  (F.obj (opposite.op (open_sets.iUnion X U))) ⟶ (@pi_opens X _ _ hp _ U F) :=  
+  (F.obj (op (open_sets.iUnion X U))) ⟶ (pi_opens X U F) :=  
 pi.lift C (λ i : I, F.map (hom_op (sset_iUnion ↑U i))) 
 
 @[hott]
-def w_res {C : Type u} [category.{v} C] [hp : has_products C]
+def w_res {C : Type u} [category.{v} C] [has_products C]
   {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) :
-  (@res X _ _ hp _ U F) ≫ (@left_res X _ _ hp _ U F) = 
-                  (@res X _ _ hp _ U F) ≫ (@right_res X _ _ hp _ U F) :=
-sorry                  
+  (res X U F) ≫ (left_res X U F) = (res X U F) ≫ (right_res X U F) :=
+let (left_comp : Π p : ↥I × I, 
+  (F.obj (op (open_sets.iUnion X U))) ⟶ (F.obj (op (U p.1 ∩ U p.2)))) := 
+  λ p : ↥I × I, F.map (hom_op (sset_iUnion ↑U p.1) ≫ 
+                       hom_op (inter_sset_l (↑(U p.1)) (↑(U p.2)))) in 
+calc (res X U F) ≫ (left_res X U F) = (res X U F) ≫ (right_res X U F) : sorry                  
 
-/-
 @[hott]
 def sheaf_condition_equalizer_products.fork {C : Type u} [category.{v} C] 
   [hp : has_products C] {I : Set} (U : I -> (open_sets X).carrier) 
-  (F : presheaf X C) : fork (@left_res X _ _ hp _ U F) 
-                            (@right_res X _ _ hp _ U F) :=
-sorry -/                           
+  (F : presheaf X C) : fork (left_res X U F) (right_res X U F) :=
+sorry                            
 
 end topology
 
