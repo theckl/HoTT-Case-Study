d275654a7af3d03b9cf80af29c1e272d4bd04740
theckl <thomas.eckl@liv.ac.uk>
2021-03-27 23:28:13 +0000

    Update Top_sheaves.lean

--- src/topology/category/Top_sheaves.lean
+++ src/topology/category/Top_sheaves.lean
@@ -32,41 +32,39 @@ def open_sets : Subset (𝒫 ↥X) :=
   {U ∈ (𝒫 ↥X) | prop_ulift (is_open ↥X U)}
 
 @[hott]
-protected def open_sets.inter (U V : (open_sets X).carrier) : 
-  (open_sets X).carrier :=
+instance Subset_Set_to_Set : has_coe_to_sort (Subset (𝒫 ↥X)) :=
+  has_coe_to_sort.mk (Type (u+1)) (λ T, T.carrier)
+
+@[hott]
+protected def open_sets.inter (U V : open_sets X) : open_sets X :=
 have U_is_open : is_open ↥X U, from ulift.down U.2, 
 have V_is_open : is_open ↥X V, from ulift.down V.2,
 have inter_is_open : prop_ulift (is_open ↥X (↑U ∩ ↑V)), from 
   ulift.up (is_open_inter ↥X U_is_open V_is_open),
 elem_pred.{u+1} (↑U ∩ ↑V) inter_is_open  
 
-#check open_sets.inter
-#print instances has_coe_to_sort
-
 @[hott, instance]
-def open_sets_inter : has_inter (open_sets X).carrier :=
+def open_sets_inter : has_inter (open_sets X) :=
 ⟨open_sets.inter X⟩    
 
 @[hott]
-def open_sets.iUnion {I : Set} (f : I -> (open_sets X).carrier) :
-  (open_sets X).carrier :=
-let f' := λ i : I, (↑(f i) : Subset X.carrier) in 
-have U_i_is_open : ∀ i : I, X.top_str.is_open (f' i), from 
+def open_sets.iUnion {I : Set} (f : I -> open_sets X) : (open_sets X) :=
+have U_i_is_open : ∀ i : I, is_open ↥X (f i), from 
   assume i, ulift.down (f i).2, 
-have open_union : X.top_str.is_open (⋃ᵢ f'), from
-  X.top_str.is_open_iUnion I f' U_i_is_open,  
-elem_pred.{u+1} (⋃ᵢ f') (ulift.up open_union)    
+have open_union : is_open ↥X (⋃ᵢ ↑f), from
+  is_open_iUnion ↥X U_i_is_open,  
+elem_pred.{u+1} (⋃ᵢ ↑f) (ulift.up open_union)    
 
 /- As a subset of the power set the set of open sets automatically receives 
    a precategory instance. Therefore, we can define a presheaf over a 
    topological space with values in a category `C` as follows: -/
 @[hott]
-def presheaf (C : Type u) [category.{v} C] := (↥(open_sets X))ᵒᵖ ⥤ C
+def presheaf (C : Type u) [category.{v} C] := (open_sets X)ᵒᵖ ⥤ C
 
 /- The product of the sections of a presheaf over a family of open sets. -/
 @[hott]
 def pi_opens {C : Type u} [category.{v} C] [has_products C]
-  {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : C :=   
+  {I : Set} (U : I -> open_sets X) (F : presheaf X C) : C :=   
 have hls : has_limits_of_shape (discrete I) C, from 
   has_products.has_limit_of_shape C I, 
 ∏ (λ i : I, F.obj (op (U i)))
@@ -75,33 +73,30 @@ have hls : has_limits_of_shape (discrete I) C, from
    of a family of open sets.-/
 @[hott]
 def pi_inters {C : Type u} [category.{v} C] [has_products C]
-  {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : C :=
+  {I : Set} (U : I -> open_sets X) (F : presheaf X C) : C :=
 have hls : has_limits_of_shape (discrete (I × I)) C, from 
   has_products.has_limit_of_shape C (I × I),  
 ∏ (λ p : ↥(I × I), F.obj (op (U p.1 ∩ U p.2)))
 
 @[hott]
 def left_res {C : Type u} [category.{v} C] [has_products C]
-  {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : 
+  {I : Set} (U : I -> open_sets X) (F : presheaf X C) : 
   (pi_opens X U F) ⟶ (pi_inters X U F) :=
 pi.lift C (λ p : ↥I × I, pi.π _ p.1 ≫ 
                             F.map (hom_op (inter_sset_l (U p.1) (U p.2))))
 
 @[hott]
 def right_res {C : Type u} [category.{v} C] [has_products C]
-  {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) : 
+  {I : Set} (U : I -> open_sets X) (F : presheaf X C) : 
   (pi_opens X U F) ⟶ (pi_inters X U F) :=
 pi.lift C (λ p : ↥I × I, pi.π _ p.2 ≫ 
                             F.map (hom_op (inter_sset_r (U p.1) (U p.2))))
 
 @[hott]
 def res {C : Type u} [category.{v} C] [has_products C]
-  {I : Set} (U : I -> (open_sets X).carrier) (F : presheaf X C) :
+  {I : Set} (U : I -> open_sets X) (F : presheaf X C) :
   (F.obj (op (open_sets.iUnion X U))) ⟶ (pi_opens X U F) :=  
 pi.lift C (λ i : I, F.map (hom_op (sset_iUnion ↑U i))) 
-
-#print instances has_coe
-set_option pp.universes true
 
 @[hott]
 def w_res {C : Type u} [category.{v} C] [has_products C]
