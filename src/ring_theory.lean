93ceabf6731c7ec6488ea3feae2436396b6ec0de
theckl <thomas.eckl@liv.ac.uk>
2021-04-02 23:39:58 +0100

    Update ring_theory.lean

--- src/ring_theory.lean
+++ src/ring_theory.lean
@@ -17,20 +17,23 @@ structure CommRing :=
 instance has_coe_CommRing : has_coe_to_sort CommRing.{u} :=
   ⟨Type u, CommRing.carrier⟩
 
-attribute [instance] Ring.struct
+attribute [instance] CommRing.struct
 
 @[hott]
-structure monoid_hom (M N : Monoid.{u}) :=
+structure ring_hom (M N : CommRing.{u}) :=
   (to_fun : M -> N)
   (map_one : to_fun 1 = 1)
   (map_mul : ∀ a b : M, to_fun (a * b) = to_fun a * to_fun b)
+  (map_zero : to_fun 0 = 0)
+  (map_add : ∀ a b : M, to_fun (a + b) = to_fun a + to_fun b)
 
-infixr ` →* `:25 := monoid_hom
+infixr ` →+* `:25 := ring_hom
 
 @[hott]
-def monoid_hom_is_set (M N : Monoid) : is_set (M →* N) :=
-  have set_eq_eq : ∀ (g h : M →* N) (p q : g = h), p = q, from
-    begin intros g h p q, hinduction p, hinduction g, sorry end, 
+def monoid_hom_is_set (M N : CommRing) : is_set (M →+* N) :=
+  have set_eq_eq : ∀ (g h : M →+* N) (p q : g = h), p = q, from
+    begin intros g h p q, hinduction p, 
+    hinduction g, sorry end, 
   is_set.mk _ set_eq_eq 
 
 end algebra
