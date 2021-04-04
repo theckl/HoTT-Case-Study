7fbb9fd2cc36bd9b347162c9760ca24e0db77911
theckl <thomas.eckl@liv.ac.uk>
2021-04-04 23:11:11 +0100

    Update ring_theory.lean

--- src/ring_theory.lean
+++ src/ring_theory.lean
@@ -40,17 +40,36 @@ def ring_hom_eta {M N : CommRing} (f : M →+* N) :
 begin hinduction f, refl end
 
 @[hott]
+def rh_fun_eq {M N : CommRing} {f g : M →+* N} (p : f = g) : 
+  f.to_fun = g.to_fun :=
+ap ring_hom.to_fun p 
+
+@[hott]
+def rh_map_one_eq {M N : CommRing} {f g : M →+* N} (p : f = g) :
+  f.map_one =[rh_fun_eq p; λ h : M -> N, h 1 = 1] g.map_one :=
+pathover_ap (λ h : M -> N, h 1 = 1) ring_hom.to_fun 
+                               (@apd (M →+* N) _ _ _ ring_hom.map_one p)  
+
+@[hott]
 def ring_hom_eq_eta {M N : CommRing} {f g : M →+* N} (p : f = g) :
-  let pA := ap ring_hom.to_fun p, 
-      map_one := (ring_hom.map_one : Π h : M →+* N, h 1 = 1) in
+  let p_fun := ap ring_hom.to_fun p, 
+      p_map_one := pathover_ap (λ h : M -> N, h 1 = 1) ring_hom.to_fun 
+                               (@apd (M →+* N) _ _ _ ring_hom.map_one p),
+      p_map_mul := pathover_ap (λ h : M -> N, ∀ a b : M, h (a * b) = h a * h b) 
+                   ring_hom.to_fun (@apd (M →+* N) _ _ _ ring_hom.map_mul p),
+      p_map_zero := pathover_ap (λ h : M -> N, h 0 = 0) ring_hom.to_fun 
+                                (@apd (M →+* N) _ _ _ ring_hom.map_zero p),
+      p_map_add := pathover_ap (λ h : M -> N, ∀ a b : M, h (a + b) = h a + h b) 
+                   ring_hom.to_fun (@apd (M →+* N) _ _ _ ring_hom.map_add p) in
   p = (ring_hom_eta f) ⬝ 
-      (apd01111 ring_hom.mk pA (apd map_one pA))
+      (apd01111 ring_hom.mk (rh_fun_eq p) (rh_map_one_eq p) p_map_mul p_map_zero p_map_add) ⬝ 
+      (ring_hom_eta g)⁻¹ :=
+begin hinduction p, hinduction f, hsimp end 
 
 @[hott, instance]
 def ring_hom_is_set (M N : CommRing) : is_set (M →+* N) :=
   have set_eq_eq : ∀ (g h : M →+* N) (p q : g = h), p = q, from
-    begin intros g h p q, hinduction p, 
-    hinduction g, sorry end, 
+    begin intros g h p q, rwr ring_hom_eq_eta p, sorry end, 
   is_set.mk _ set_eq_eq 
 
 end algebra
