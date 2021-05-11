2ea50b20ce178bd808bf34dd935e768c50c179b8
theckl <thomas.eckl@liv.ac.uk>
2021-05-11 22:34:49 +0100

    Update ring_theory.lean

--- src/ring_theory.lean
+++ src/ring_theory.lean
@@ -4,7 +4,8 @@ universes u v w
 hott_theory
 
 namespace hott
-open hott.is_trunc hott.is_equiv hott.algebra hott.set subset categories hott.category_theory.limits
+open hott.is_trunc hott.is_equiv hott.algebra hott.set subset categories 
+     hott.category_theory.limits hott.sigma
 
 namespace algebra
 
@@ -459,25 +460,16 @@ begin
 
 @[hott]
 def limit_comm_ring {J : Set.{v}} [precategory.{v} J] (F : J ⥤ CommRing.{v}) :
-  comm_ring_str.P (get_limit_cone (forget F)).cone.X :=
+  comm_ring_str.P (set_limit_cone (forget F)).cone.X :=
 begin    
   exact @comm_subring (CommRing_product F.obj) (ring_limit_pred F) (ring_pred_is_closed F)
 end    
 
 @[hott]
-def CommRing_limit.cone {J : Set.{v}} [precategory.{v} J] (F : J ⥤ CommRing.{v}) : 
-  CommRing :=
-CommRing.mk (limit.cone (forget F)).X (limit_comm_ring F)  
-
-#check is_limit.lift
-#check is_ring_hom.mk
-#print fields comm_ring
-
-@[hott]
 def CommRing_limit_cone {J : Set.{v}} [precategory.{v} J] (F : J ⥤ CommRing.{v}) : 
   limit_cone F :=
 begin 
-  fapply str_limit_cone F, 
+  fapply str_limit_cone (set_limit_cone (forget F)), 
   fapply limit_cone_str_data.mk,
   { exact limit_comm_ring F }, --lc_str
   { intro j, 
@@ -487,20 +479,35 @@ begin
     { exact comm_subring_embed_hom (ring_limit_pred F) },
     { exact CommRing_product_proj_hom F.obj j } }, --lc_legs_H
   { intro s, fapply is_ring_hom.mk, 
-    { have p : get_limit_cone (forget F) = set_limit_cone (forget F), from
-        limit_cone_is_unique (forget F) _ _,
-      have q : (get_limit_cone (forget F)).is_limit.lift 
-                 =[p; λ lc : limit_cone (forget F), Π s : cone (forget F), s.X ⟶ lc.cone.X] 
-                   (set_limit_cone (forget F)).is_limit.lift, from 
-        apd (λ lc : limit_cone (forget F), lc.is_limit.lift) p,  
-      rwr eq_tr_of_pathover q,
-      /- comm_ring.one ↥((get_limit_cone (forget F)).cone.X -/
-      /- change (λ j, ((s.π.app j).1 (1 : s.X) : (F.obj j).carrier)) = (λ j, (1 : F.obj j)),-/ 
-      sorry },
-    { sorry },
-    { sorry },
-    { sorry } } --lift_H
+    { fapply sigma_eq,
+      { change (λ j, (s.π.app j).1 (1 : s.X.carrier)) = (λ j, (1 : F.obj j)),
+        apply eq_of_homotopy, intro j, hsimp, exact (s.π.app j).2.map_one }, 
+      { apply pathover_of_tr_eq, apply is_prop.elim } },
+    { intros t₁ t₂, fapply sigma_eq, 
+      { change (λ j, (s.π.app j).1 (t₁ * t₂)) = 
+                              (λ j, (((s.π.app j).1 t₁) * ((s.π.app j).1 t₂) : F.obj j)),
+        apply eq_of_homotopy, intro j, hsimp, exact (s.π.app j).2.map_mul t₁ t₂ },
+      { apply pathover_of_tr_eq, apply is_prop.elim } },  
+    { fapply sigma_eq,
+      { change (λ j, (s.π.app j).1 (0 : s.X.carrier)) = (λ j, (0 : F.obj j)),
+        apply eq_of_homotopy, intro j, hsimp, exact (s.π.app j).2.map_zero }, 
+      { apply pathover_of_tr_eq, apply is_prop.elim } },    
+    { intros t₁ t₂, fapply sigma_eq, 
+      { change (λ j, (s.π.app j).1 (t₁ + t₂)) = 
+                              (λ j, (((s.π.app j).1 t₁) + ((s.π.app j).1 t₂) : F.obj j)),
+        apply eq_of_homotopy, intro j, hsimp, exact (s.π.app j).2.map_add t₁ t₂ },
+      { apply pathover_of_tr_eq, apply is_prop.elim } } } --lift_H
 end   
+
+@[hott]
+def CommRing_has_limit {J : Set.{v}} [precategory.{v} J] (F : J ⥤ CommRing.{v}) : 
+  has_limit F :=
+has_limit.mk (CommRing_limit_cone F)
+
+@[hott, instance]
+def CommRing_has_limits_of_shape {J : Set.{v}} [precategory.{v} J] :
+  has_limits_of_shape J CommRing.{v} :=
+has_limits_of_shape.mk (λ F, CommRing_has_limit F)   
 
 end algebra
 
