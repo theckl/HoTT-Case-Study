c204859501fabbfa214d88c4f57136bfe9ae686f
theckl <thomas.eckl@liv.ac.uk>
2021-06-05 23:39:22 +0100

    Update locally_ringed_space.lean

--- src/algebraic_geometry/locally_ringed_space.lean
+++ src/algebraic_geometry/locally_ringed_space.lean
@@ -1,4 +1,6 @@
-import topology.category.Top_sheaves
+import topology.category.Top_sheaves rings.basic rings.limits
+
+open hott.algebra
 
 universes u v w
 hott_theory
@@ -6,6 +8,10 @@ hott_theory
 namespace hott
 
 namespace algebraic_geometry
+
+@[hott]
+structure LocallyRingedSpace extends SheafedSpace CommRing :=
+(local_ring : ∀ x, local_ring (presheaf.stalk x))
 
 end algebraic_geometry
 
