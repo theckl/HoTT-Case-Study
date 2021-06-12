1cb9f9a85344eeb641664a67715c1c514710a92f
theckl <thomas.eckl@liv.ac.uk>
2021-06-12 22:36:09 +0100

    Update locally_ringed_space.lean

--- src/algebraic_geometry/locally_ringed_space.lean
+++ src/algebraic_geometry/locally_ringed_space.lean
@@ -2,21 +2,21 @@ import topology.category.Top_sheaves rings.basic rings.limits rings.colimits
 
 open hott.algebra hott.category_theory.limits hott.topology hott.category_theory.colimits
 
-universes v u w
+universes v v' u u' w
 hott_theory
 
 namespace hott
 
 namespace algebraic_geometry
 
-set_option pp.universes true
-
-#print ring_has_colimits
-
 @[hott]
-structure LocallyRingedSpace extends SheafedSpace.{u u+1 u} CommRing.{u} :=
-(local_ring : ∀ x, local_ring (@stalk.{u u+1 u} to_PresheafedSpace.carrier 
-                               CommRing _ (ring_has_colimits) to_PresheafedSpace.presheaf x))
+structure LocallyRingedSpace :=
+(carrier : Top)
+(presheaf : topology.presheaf carrier CommRing)
+(sheaf_condition : @topology.sheaf_condition carrier _ _ CommRing_has_products 
+                     (presheaf ⋙ ring_ulift_functor)) 
+(local_ring : ∀ x, local_ring (@stalk carrier CommRing _ 
+                     ring_has_colimits (presheaf ⋙ ring_ulift_functor) x))
 
 end algebraic_geometry
 
