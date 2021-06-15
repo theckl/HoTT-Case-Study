fb76baa5d8d283c8ec16cbe80f9181fe9a33f1e9
theckl <thomas.eckl@liv.ac.uk>
2021-06-15 23:20:27 +0100

    Update modules.lean

--- src/rings/modules.lean
+++ src/rings/modules.lean
@@ -7,19 +7,19 @@ set_option old_structure_cmd true
 
 namespace hott
 
-open hott.algebra
+open hott.algebra hott.subset
 
 namespace algebra
 
 @[hott]
-structure module (R : CommRing) (M : Set) extends ab_group M renaming 
+structure module_str (R : CommRing) (M : Set) extends ab_group M renaming 
   mul→add mul_assoc→add_assoc one→zero one_mul→zero_add mul_one→add_zero inv→neg 
   mul_left_inv→add_left_inv mul_comm→add_comm :=
 (smul : R -> M -> M)  
 (smul_assoc : ∀ (r s : R) (m : M), smul r (smul s m) = smul (r * s) m)
-(smul_distrib_left : ∀ (r s : R) (m : M), smul (r + s) m = 
+(add_smul : ∀ (r s : R) (m : M), smul (r + s) m = 
                                                     add (smul r m) (smul s m))
-(smul_distrib_right : ∀ (r : R) (m n :M), smul r (add m n) = 
+(smul_add : ∀ (r : R) (m n : M), smul r (add m n) = 
                                                     add (smul r m) (smul r n)) 
 (zero_mul : ∀ (m : M), smul 0 m = zero)
 (mul_zero : ∀ (r : R), smul r zero = zero)
@@ -27,23 +27,64 @@ structure module (R : CommRing) (M : Set) extends ab_group M renaming
 /- I don't know why `0` and `+` don't work for `M`. An infix notation for `smul` 
    would exist if we used `has_scalar`. To simplify things we introduce it later. -/                                                                                                   
 
-attribute [class] module
+attribute [class] module_str
 
+/- If we need the category of R-modules we need to change this definition to a 
+   structure on the category of Sets. -/
 @[hott]
-structure RingModule (R : CommRing) :=
+structure Module (R : CommRing) :=
   (carrier : Set)
-  (str : module R carrier)
+  (str : module_str R carrier)  
 
 @[hott]
-instance RingModule_to_Set (R : CommRing) : has_coe (RingModule R) Set :=
-  ⟨λ M : RingModule R, M.carrier⟩
+instance Module_to_Set (R : CommRing) : has_coe (Module R) Set :=
+  ⟨λ M : Module R, M.carrier⟩
 
 @[hott]
-instance RingModule_to_Type (R : CommRing) : has_coe_to_sort (RingModule R) :=
-  has_coe_to_sort.mk (Type _) (λ M : RingModule R, M.carrier)  
+instance Module_to_Type (R : CommRing) : has_coe_to_sort (Module R) :=
+  has_coe_to_sort.mk (Type _) (λ M : Module R, M.carrier)  
 
 @[hott]
-instance (R : CommRing) (M : RingModule R) : module R M.carrier := M.str   
+instance (R : CommRing) (M : Module R) : module_str R M.carrier := M.str   
+
+@[hott, instance]
+def module_add {R : CommRing} {M : Module R} : has_add M :=
+⟨M.str.add⟩
+
+/- There is a better palce for this definition and notation. -/
+@[hott]
+class has_scalar (α β : Type _) := (smul : α -> β -> β)
+
+hott_theory_cmd "local infix `•`:200 := hott.algebra.has_scalar.smul"
+
+@[hott, instance]
+def module_smul {R : CommRing} {M : Module R} : has_scalar R M :=
+⟨M.str.smul⟩
+
+/- A commutative ring `R` is an `R`-module. -/
+@[hott]
+def ring_as_module (R : CommRing) : Module R :=
+  sorry
+
+@[hott]
+structure submodule_str {R : CommRing} (M : Module R) (N : Subset M.carrier) :=
+  (add_closed : ∀ n₁ n₂ : M, n₁ ∈ N -> n₂ ∈ N -> (n₁ + n₂) ∈ N)
+  (smul_closed : ∀ (r : R) (n : M), n ∈ N -> r • n ∈ N)
+
+/- At some point we may need the module structure on a submodule. -/
+
+@[hott]
+structure Submodule (R : CommRing) (M : Module R) :=
+  (carrier : Subset M.carrier)
+  (str : submodule_str M carrier)
+
+@[hott]
+instance Module_to_Subset (R : CommRing) (M : Module R) : 
+  has_coe (Submodule R M) (Subset M.carrier) :=
+  ⟨λ N : Submodule R M, N.carrier⟩
+
+@[hott]
+def Ideal (R : CommRing) := Submodule R (ring_as_module R)
 
 end algebra
 
