c19dacb4f46fc4e26232d8696da24bbc0394ed2b
theckl <thomas.eckl@liv.ac.uk>
2021-06-21 23:46:51 +0100

    Update spectrum.lean

--- src/algebraic_geometry/spectrum.lean
+++ src/algebraic_geometry/spectrum.lean
@@ -7,7 +7,7 @@ set_option old_structure_cmd true
 
 namespace hott
 
-open hott.algebra hott.subset 
+open hott.algebra hott.subset hott.trunc
 
 namespace algebraic_geometry
 
@@ -20,5 +20,16 @@ def prime_spectrum (R : CommRing) := PrimeIdeal_Set R
 def zero_locus_pred (R : CommRing) : 
   Ideal_Set R -> Setpred (prime_spectrum R) :=
 λ I P, prop_ulift (I.carrier ⊆ P.1.carrier)
+
+@[hott]
+def is_Zariski_closed {R : CommRing} : Subset (prime_spectrum R) -> Prop :=
+  λ Z, image (zero_locus_pred R) (sset_to_pred Z)
+
+@[hott]
+def empty_Zariski_closed (R : CommRing) : 
+  is_Zariski_closed (empty_Subset (prime_spectrum R)) :=
+begin 
+  apply tr, sorry
+end      
 
 end hott
