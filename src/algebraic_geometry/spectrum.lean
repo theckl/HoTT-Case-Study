e02c55b51e2deaff9c1bc7383e7274f203ace4ec
theckl <thomas.eckl@liv.ac.uk>
2021-06-22 23:54:23 +0100

    Update spectrum.lean

--- src/algebraic_geometry/spectrum.lean
+++ src/algebraic_geometry/spectrum.lean
@@ -29,7 +29,33 @@ def is_Zariski_closed {R : CommRing} : Subset (prime_spectrum R) -> Prop :=
 def empty_Zariski_closed (R : CommRing) : 
   is_Zariski_closed (empty_Subset (prime_spectrum R)) :=
 begin 
-  apply tr, sorry
-end      
+  apply tr, 
+  have all_empty : zero_locus_pred R (R•1) = 
+                   (sset_to_pred (empty_Subset (prime_spectrum R))), from 
+    begin 
+      apply eq_of_homotopy, intro P, 
+      change prop_ulift ((R•1).carrier ⊆ PrimeIdeal_to_Subset P) =
+                          (sset_to_pred (empty_Subset (prime_spectrum R)) P),
+      rwr Not_eq_False (proper_prime_ideal P), 
+      rwr Not_eq_False (empty_not_elem P), exact ulift_False
+    end,
+  exact ⟨R•1, all_empty⟩
+end 
+
+@[hott]
+def union_Zariski_closed (R : CommRing) : Π U V : Subset (prime_spectrum R), 
+  is_Zariski_closed U -> is_Zariski_closed V -> is_Zariski_closed (U ∪ V) :=
+begin  
+  intros U V clU clV, hinduction clU with IU, hinduction clV with IV, apply tr,
+  have union_inter : zero_locus_pred R (IU.1 ∩ IV.1) = (sset_to_pred (U ∪ V)), from
+    begin
+      apply eq_of_homotopy, intro P, 
+      change prop_ulift ((IU.point ∩ IV.point).carrier ⊆ PrimeIdeal_to_Subset P) =
+        P∈(U ∪ V),
+         
+      sorry
+    end,
+  exact ⟨IU.1 ∩ IV.1, union_inter⟩
+end    
 
 end hott
