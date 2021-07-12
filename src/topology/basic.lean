fc2fd71d6cd86cf8866e7a090e9b8ab1f7eb90b4
theckl <thomas.eckl@liv.ac.uk>
2021-07-12 22:45:20 +0100

    Update basic.lean

--- src/topology/basic.lean
+++ src/topology/basic.lean
@@ -1,4 +1,4 @@
-import setalgebra
+import sets.setalgebra
 
 universes u v w
 hott_theory
@@ -6,15 +6,17 @@ hott_theory
 namespace hott
 open hott.set hott.subset 
 
+set_option pp.universes true
+
 /- A topology on the Set [T]. -/
-variable T : Set
+variable T : Set.{u}
 
 @[hott]
 structure topological_space :=
-(is_open        : Subset T → trunctype -1)
+(is_open        : Subset.{u u} T → trunctype -1)
 (is_open_univ   : is_open (total_Subset T))
 (is_open_inter  : ∀ U V : Subset T, is_open U → is_open V → is_open (U ∩ V)) 
-(is_open_iUnion : ∀ (I : Set) (f : I -> 𝒫 T), (∀ i : I, is_open (f i)) -> 
+(is_open_iUnion : ∀ (I : Set.{u}) (f : I -> Powerset.{u u} T), (∀ i : I, is_open (f i)) -> 
                                                          is_open (⋃ᵢ f)) 
 /- (is_open_sUnion : ∀ S : Subset (𝒫 T), (∀U, U ∈ S -> is_open U) → is_open (⋃₀ S)) 
 
