6ed482875e56e0cbc2c5a9b882269a888c7e3dfe
theckl <thomas.eckl@liv.ac.uk>
2021-08-20 23:22:12 +0100

    Update types2.lean

--- src/types2.lean
+++ src/types2.lean
@@ -1,14 +1,23 @@
-import hott.init
+import hott.init hott.hit.trunc hott.types.trunc
 
 universes u v w
 hott_theory
 
 namespace hott
+open hott.is_trunc hott.trunc
 
 /- Properties of the product of two types -/
 @[hott]
 def all_prod_all {I J : Type _} (F : I -> J -> Type _) : 
   (∀ p : I × J, F p.1 p.2) -> ∀ (i : I) (j : J), F i j :=
 assume all_prod i j, all_prod ⟨i,j⟩  
+
+#print trunc.elim
+
+/- An extended elimination principle for truncations -/
+@[hott]
+def trunc.elim2 {n : ℕ₋₂} {A : Type _} {B : Type _} {P : Type _} [Pt : is_trunc n P] : 
+  (A → B -> P) → trunc n A → trunc n B -> P :=
+begin intros f tA tB, exact untrunc_of_is_trunc (trunc_functor2 f tA tB) end  
 
 end hott
