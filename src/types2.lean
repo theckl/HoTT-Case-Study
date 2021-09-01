069db53c5c0d600f8b8a7c03fd46fa99fcf2b8d7
theckl <thomas.eckl@liv.ac.uk>
2021-09-01 23:21:11 +0100

    Update types2.lean

--- src/types2.lean
+++ src/types2.lean
@@ -12,8 +12,6 @@ def all_prod_all {I J : Type _} (F : I -> J -> Type _) :
   (∀ p : I × J, F p.1 p.2) -> ∀ (i : I) (j : J), F i j :=
 assume all_prod i j, all_prod ⟨i,j⟩  
 
-#print trunc.elim
-
 /- An extended elimination principle for truncations -/
 @[hott]
 def trunc.elim2 {n : ℕ₋₂} {A : Type _} {B : Type _} {P : Type _} [Pt : is_trunc n P] : 
@@ -27,5 +25,39 @@ begin
   intro p, fapply sigma.sigma_eq, 
   exact p, apply pathover_of_tr_eq, exact is_prop.elim _ _ 
 end  
+
+/- The decode-encode technique for sums; it is contained in [types.sum] from the HoTT3 
+   library, but this file does not compile. -/
+@[hott, reducible, hsimp] 
+def sum_code {A : Type.{u}} {B : Type.{v}} : A ⊎ B → A ⊎ B → Type (max u v) :=
+  begin
+    intros x y, induction x with a b; induction y with a' b',
+    exact ulift.{(max u v) u} (a = a'), exact ulift empty,
+    exact ulift empty, exact @ulift.{(max u v) v} (b = b')
+  end   
+
+@[hott] 
+def sum_decode {A : Type.{u}} {B : Type.{v}} : 
+  Π (z z' : A ⊎ B), sum_code z z' → z = z' :=
+  begin
+    intros z z', induction z with a b; induction z' with a' b',
+    exact λ c, ap sum.inl (ulift.down c),
+    exact λ c, empty.elim (ulift.down c),
+    exact λ c, empty.elim (ulift.down c),
+    exact λ c, ap sum.inr (ulift.down c)
+  end
+
+@[hott] 
+def sum_encode {A : Type.{u}} {B : Type.{v}} {z z' : A ⊎ B} (p : z = z') : sum_code z z' :=
+  by induction p; induction z; exact ulift.up idp
+
+@[hott] 
+def sum_eq_equiv {A : Type.{u}} {B : Type.{v}} {z z' : A ⊎ B} : (z = z') ≃ sum_code z z' :=
+  begin
+    fapply equiv.MK, apply sum_encode, apply sum_decode,
+    { induction z with a b; induction z' with a' b';
+       intro b; induction b with b; induction b; reflexivity },
+    { intro p, induction p, induction z; refl  }
+  end
 
 end hott
