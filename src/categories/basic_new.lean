9166e9623f9372b58851306e5877bf0524731b12
theckl <thomas.eckl@liv.ac.uk>
2023-03-07 23:19:20 +0000

    Create basic_new.lean

---
+++ src/categories/basic_new.lean
@@ -0,0 +1,284 @@
+import sets.algebra init2 types2 sets.axioms
+
+universes v v' v'' v''' u u' u'' u''' w 
+hott_theory
+
+namespace hott
+open hott.eq hott.sigma hott.set hott.subset hott.is_trunc hott.is_equiv
+
+/-
+We introduce precategories and categories following the HoTT book, 
+Sec. 9.1. HoTT precategories have sets of homomorphisms, and HoTT categories 
+prescribe univalence : Isomorphisms are equivalent to identities of objects.
+
+As far as possible we copy the mathlib-code in [category_theory.category.default]. In particular,
+we keep the distinction of universe levels for objects and morphisms of a category.
+-/
+
+namespace precategories
+
+/-- A 'notation typeclass' on the way to defining a precategory. -/
+@[hott]
+class has_hom (obj : Type u) : Type (max u (v+1)) :=
+  (hom : obj → obj → Set.{v})
+
+infixr ` ⟶ `:10 := has_hom.hom  -- type as \h
+
+/-- A preliminary structure on the way to defining a precategory,
+containing the data, but none of the axioms. -/
+@[hott]
+class category_struct (obj : Type u) 
+extends has_hom.{v} obj : Type (max u (v+1)) :=
+(id       : Π a : obj, hom a a)
+(comp     : Π {a b c : obj}, (a ⟶ b) → (b ⟶ c) → (a ⟶ c))
+
+notation `𝟙` := category_struct.id -- type as \b1
+infixr ` ≫ `:80 := category_struct.comp -- type as \gg
+
+/-- The structure of a precategory. -/
+@[hott, class]
+structure is_precat (obj : Type u) 
+extends category_struct.{v} obj : Type (max u (v+1)) :=
+(id_comp : ∀ {a b : obj} (f : hom a b), 𝟙 a ≫ f = f)
+(comp_id : ∀ {a b : obj} (f : hom a b), f ≫ 𝟙 b = f)
+(assoc   : ∀ {a b c d : obj} (f : hom a b) (g : hom b c) (h : hom c d),
+  (f ≫ g) ≫ h = f ≫ (g ≫ h))
+
+attribute [hsimp] is_precat.id_comp is_precat.comp_id is_precat.assoc
+
+@[hott]
+structure Precategory :=
+  (obj : Type u)
+  (struct : is_precat obj)
+
+@[hott] instance : has_coe_to_sort Precategory := 
+  has_coe_to_sort.mk Type.{u} Precategory.obj
+
+attribute [instance] Precategory.struct
+
+end precategories
+
+open precategories
+namespace categories
+
+/- Definition of categorical isomorphisms. -/
+@[hott]
+structure iso {C : Type u} [is_precat.{v} C] (a b : C) :=
+  (hom : a ⟶ b)
+  (inv : b ⟶ a) 
+  (r_inv : inv ≫ hom = 𝟙 b) 
+  (l_inv : hom ≫ inv = 𝟙 a) 
+
+postfix `⁻¹ʰ`:std.prec.max_plus := iso.inv
+
+infix ` ≅ `:25 := iso
+
+@[hott]
+structure is_iso {C : Precategory} {a b : C} (f : a ⟶ b) :=
+  (inv : b ⟶ a)
+  (r_inv : inv ≫ f = 𝟙 b)
+  (l_inv : f ≫ inv = 𝟙 a)
+
+@[hott]
+def is_iso_to_iso {C : Precategory} {a b : C} (f : a ⟶ b) 
+  (H : is_iso f) : a ≅ b := iso.mk f H.inv H.r_inv H.l_inv
+
+@[hott]
+def iso_to_is_iso {C : Precategory} {a b : C} (f : a ≅ b) : 
+  is_iso f.hom := is_iso.mk f.inv f.r_inv f.l_inv  
+
+@[hott]
+def iso.eta {C : Precategory} {a b : C} (i : a ≅ b) : 
+  i = iso.mk i.hom i.inv i.r_inv i.l_inv :=
+begin hinduction i, hsimp end  
+
+@[hott, hsimp]
+def inv_iso {C : Precategory} {a b : C} (i : a ≅ b) : b ≅ a :=
+  iso.mk i.inv i.hom i.l_inv i.r_inv
+
+/- Calculation rules for isomorphisms. -/
+@[hott, hsimp]
+def iso_inv_inv {C : Precategory} {a b : C} (i : a ≅ b) :
+  (inv_iso i)⁻¹ʰ = i.hom :=
+by hsimp 
+
+@[hott, hsimp]
+def iso_rcancel {C : Precategory} {a b c : C} (i : a ≅ b)
+  {g h : c ⟶ a} : g ≫ i.hom = h ≫ i.hom -> g = h :=
+assume pc, 
+have pc_inv : (g ≫ i.hom) ≫ i.inv = (h ≫ i.hom) ≫ i.inv, from 
+  ap (λ h : c ⟶ b, h ≫ i.inv) pc,
+calc   g = g ≫ 𝟙 a : by hsimp
+     ... = g ≫ (i.hom ≫ i.inv) : by rwr <-i.l_inv
+     ... = (g ≫ i.hom) ≫ i.inv : by hsimp
+     ... = (h ≫ i.hom) ≫ i.inv : by rwr pc_inv
+     ... = h ≫ (i.hom ≫ i.inv) : by hsimp
+     ... = h ≫ 𝟙 a : by rwr i.l_inv     
+     ... = h : by hsimp 
+
+@[hott, hsimp]
+def iso_lcancel {C : Precategory} {a b c : C} (i : a ≅ b)
+  {g h : b ⟶ c} : i.hom ≫ g = i.hom ≫ h -> g = h :=
+assume cp, 
+have cp_inv : i.inv ≫ (i.hom ≫ g) = i.inv ≫ (i.hom ≫ h), from 
+  ap (λ h : a ⟶ c, i.inv ≫ h) cp,
+calc   g = 𝟙 b ≫ g : by hsimp
+     ... = (i.inv ≫ i.hom) ≫ g : by rwr <-i.r_inv
+     ... = i.inv ≫ (i.hom ≫ g) : by hsimp
+     ... = i.inv ≫ (i.hom ≫ h) : by rwr cp_inv
+     ... = (i.inv ≫ i.hom) ≫ h : by hsimp
+     ... = 𝟙 b ≫ h : by rwr i.r_inv     
+     ... = h : by hsimp 
+
+@[hott, hsimp]
+def iso_move_lr {C : Precategory} {a b c : C} (i : a ≅ b)
+  (g : b ⟶ c) (h : a ⟶ c) : i.hom ≫ g = h -> g = i.inv ≫ h :=
+assume pcr,
+have i.inv ≫ i.hom ≫ g = i.inv ≫ h, from ap (λ h : a ⟶ c, i.inv ≫ h) pcr,
+calc g   = 𝟙 b ≫ g : by hsimp
+     ... = (i.inv ≫ i.hom) ≫ g : by rwr <-i.r_inv
+     ... = i.inv ≫ (i.hom ≫ g) : by hsimp
+     ... = i.inv ≫ h : by rwr pcr   
+
+@[hott, hsimp]
+def iso_move_rl {C : Precategory} {a b c : C} (i : a ≅ b)
+  (g : c ⟶ a) (h : c ⟶ b) : g ≫ i.hom = h -> g = h ≫ i.inv :=
+assume pcl,
+have (g ≫ i.hom) ≫ i.inv = h ≫ i.inv, from ap (λ h : c ⟶ b, h ≫ i.inv) pcl,
+calc g   = g ≫ 𝟙 a : by hsimp
+     ... = g ≫ (i.hom ≫ i.inv) : by rwr <-i.l_inv
+     ... = (g ≫ i.hom) ≫ i.inv : by hsimp
+     ... = h ≫ i.inv : by rwr pcl     
+
+/- Isomorphisms are uniquely determined by their underlying homomorphism:
+   The inverse map by functorial equalities, and the functorial equalities 
+   because the types of homomorphisms are sets. -/
+@[hott]
+def hom_eq_to_iso_eq {C : Precategory} {a b : C} {i j : a ≅ b} :
+  i.hom = j.hom -> i = j :=
+assume hom_eq, 
+have inv_eq : i.inv = j.inv, from 
+  calc i.inv = i.inv ≫ 𝟙 a : by hsimp
+       ...   = i.inv ≫ (j.hom ≫ j.inv) : by rwr j.l_inv⁻¹ 
+       ...   = (i.inv ≫ j.hom) ≫ j.inv : by hsimp
+       ...   = (i.inv ≫ i.hom) ≫ j.inv : by rwr hom_eq⁻¹
+       ...   = 𝟙 b ≫ j.inv : by rwr i.r_inv
+       ...   = j.inv : by hsimp,
+let R := λ (f : a ⟶ b) (g : b ⟶ a), g ≫ f = 𝟙 b,
+    L := λ (f : a ⟶ b) (g : b ⟶ a), f ≫ g = 𝟙 a in
+have r_inv_eq : i.r_inv =[ap011 R hom_eq inv_eq; id] j.r_inv, from 
+  begin apply pathover_of_tr_eq, apply is_set.elim end,
+have l_inv_eq : i.l_inv =[ap011 L hom_eq inv_eq; id] j.l_inv, from 
+  begin apply pathover_of_tr_eq, apply is_set.elim end, 
+calc   i = iso.mk i.hom i.inv i.r_inv i.l_inv : iso.eta i 
+     ... = iso.mk j.hom j.inv j.r_inv j.l_inv : 
+                                        ap0111 iso.mk hom_eq inv_eq r_inv_eq l_inv_eq
+     ... = j : (iso.eta j)⁻¹
+
+@[hott, hsimp]
+def id_is_iso {C : Type u} [is_precat.{v} C] (a : C) : a ≅ a := 
+  have inv_eq : 𝟙 a ≫ 𝟙 a = 𝟙 a, by hsimp,
+  iso.mk (𝟙 a) (𝟙 a) inv_eq inv_eq
+
+@[hott, hsimp]
+def idtoiso {C : Type u} [is_precat.{v} C] {a b : C} : (a = b) -> (a ≅ b) :=
+  begin intro eq, exact eq ▸[λ c, a ≅ c] id_is_iso a end
+
+/- `idtoiso` is natural. -/
+@[hott, hsimp]
+def idtoiso_refl_eq {C : Precategory} (a : C) : idtoiso (refl a) = id_is_iso a :=
+  by hsimp
+
+@[hott]
+def id_inv_iso_inv {C : Precategory} {c₁ c₂ : C} (p : c₁ = c₂) :
+  idtoiso p⁻¹ = inv_iso (idtoiso p) := 
+begin hinduction p, refl end 
+
+/- The next two facts correspond to [HoTT-Book, Lem.9.1.9]. -/
+@[hott]
+def id_hom_tr_comp {C : Precategory} {c₁ c₂ d : C} (p : c₁ = c₂)
+  (h : c₁ ⟶ d) : p ▸ h = (idtoiso p)⁻¹ʰ ≫ h :=
+begin hinduction p, hsimp end   
+
+@[hott]
+def id_hom_tr_comp' {C : Precategory} {c₁ c₂ d : C} (p : c₁ = c₂)
+  (h : d ⟶ c₁) : p ▸ h = h ≫ (idtoiso p).hom :=
+begin hinduction p, hsimp end
+
+/-- The structure of a category. -/
+@[hott]
+class is_cat (obj : Type u) extends is_precat.{v} obj :=
+  (ideqviso : ∀ a b : obj, is_equiv (@idtoiso _ _ a b)) 
+
+attribute [instance] is_cat.ideqviso
+
+@[hott]
+structure Category :=
+  (obj : Type u)
+  (struct : is_cat obj)
+/-
+@[hott, instance]
+def Cat.to_obj : has_coe_to_sort Category := 
+  has_coe_to_sort.mk Type.{u} Category.obj
+-/
+@[hott, instance]
+def Cat.to_Precat : has_coe Category Precategory := 
+  has_coe.mk (λ C : Category, Precategory.mk C.obj (C.struct.to_is_precat))
+
+attribute [instance] Category.struct
+
+@[hott, hsimp]
+def category.isotoid {C : Category} : 
+  Π {a b : C}, a ≅ b -> a = b :=
+assume a b iso,  
+@is_equiv.inv _ _ (@idtoiso C.obj _ a b) (is_cat.ideqviso a b) iso  
+
+@[hott, hsimp]
+def category.idtoiso_rinv {C : Category} {a b : C} :
+  ∀ i : a ≅ b, idtoiso (idtoiso⁻¹ᶠ i) = i :=
+is_equiv.right_inv (@idtoiso _ _ a b) 
+
+@[hott, hsimp]
+def category.idtoiso_linv {C : Category} {a b : C} :
+  ∀ p : a = b, idtoiso⁻¹ᶠ (idtoiso p) = p :=
+is_equiv.left_inv (@idtoiso _ _ a b) 
+
+@[hott, hsimp]
+def category.idtoiso_rinv' {C : Category} {a b : C} :
+  ∀ i : a ≅ b, idtoiso (category.isotoid i) = i :=
+is_equiv.right_inv (@idtoiso _ _ a b) 
+
+@[hott, hsimp]
+def category.idtoiso_linv' {C : Category} {a b : C} :
+  ∀ p : a = b, category.isotoid (idtoiso p) = p :=
+is_equiv.left_inv (@idtoiso _ _ a b) 
+
+@[hott]
+def isotoid_id_refl {C : Category} :
+  Π (a : C), category.isotoid (id_is_iso a) = refl a :=
+begin 
+  intro a, rwr <- idtoiso_refl_eq  a, 
+  exact category.idtoiso_linv (refl a) 
+end 
+
+@[hott]
+def iso_hom_tr_comp {C : Category} {c₁ c₂ d : C} (i : c₁ ≅ c₂)
+  (h : c₁ ⟶ d) : (idtoiso⁻¹ᶠ i) ▸ h = i⁻¹ʰ ≫ h :=
+begin 
+  rwr <-(category.idtoiso_rinv i),  
+  rwr category.idtoiso_linv (idtoiso⁻¹ᶠ i),
+  exact id_hom_tr_comp (idtoiso⁻¹ᶠ i) h
+end 
+
+@[hott]
+def iso_hom_tr_comp' {C : Category} {c₁ c₂ d : C} (i : c₁ ≅ c₂)
+  (h : d ⟶ c₁) : (idtoiso⁻¹ᶠ i) ▸ h = h ≫ i.hom :=
+begin 
+  rwr <-(category.idtoiso_rinv i),  
+  rwr category.idtoiso_linv (idtoiso⁻¹ᶠ i),
+  exact id_hom_tr_comp' (idtoiso⁻¹ᶠ i) h
+end
+
+end categories
+
+end hott
