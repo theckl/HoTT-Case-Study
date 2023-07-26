3f2390173bc461da265664b19202ec812ccc6beb
theckl <thomas.eckl@liv.ac.uk>
2023-07-26 12:50:27 +0100

    Create ring.lean

---
+++ src/algebra/ring.lean
@@ -0,0 +1,15 @@
+import categories.models
+
+universes v v' u u' w 
+hott_theory
+
+namespace hott
+open signature signature.term signature.formula precategories categories.limits set 
+     subset categories.colimits categories.pullbacks categories.inf_pullbacks
+     categories.boolean trunc is_trunc hott.set
+
+namespace rings
+
+end rings
+
+end hott
