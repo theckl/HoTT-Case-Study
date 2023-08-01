48edd5f521af47bf795439bb661a034532b06393
theckl <thomas.eckl@liv.ac.uk>
2023-08-01 13:07:26 +0100

    Update ring.lean

--- src/algebra/ring.lean
+++ src/algebra/ring.lean
@@ -1,14 +1,35 @@
-import categories.models
+import sets.basic categories.models
 
 universes v v' u u' w 
 hott_theory
 
 namespace hott
-open signature signature.term signature.formula precategories categories.limits set 
-     subset categories.colimits categories.pullbacks categories.inf_pullbacks
-     categories.boolean trunc is_trunc hott.set
+
+open signature hott.set
 
 namespace rings
+
+/- At the moment, we only need commutative rings with one. -/
+
+@[hott]
+def ring_signature : fo_signature :=
+begin
+  fapply fo_signature.mk,
+  exact nat_Set, -- let's see whether this is easier than introducing strings
+  exact fin_Set 1, --one sort
+  exact fin_Set 5, --five operations : add, mult, zero, one, neg
+  exact fin_map_of_list [fin_Set 2, fin_Set 2, fin_Set 0, fin_Set 0, fin_Set 1], 
+                                                            --arities of operations
+  exact λ o a, ⟨0, nat.zero_lt_succ 0⟩, --sources and targets of operations can only                                     
+  exact λ o, ⟨0, nat.zero_lt_succ 0⟩,   --have one sort
+  exact fin_Set 0, --no extra relations : the laws for addition and multiplication are
+                   --                     axioms of the theory of rings 
+  intro f, hinduction nat.not_lt_zero f.1 f.2, --no arities of relations needed 
+  exact λ r a, ⟨0, nat.zero_lt_succ 0⟩, --no sources of relations exist 
+  exact fin_Set 0,  --no infinite dis-/conjunctions needed
+  intro ind, hinduction nat.not_lt_zero ind.1 ind.2
+end
+
 
 end rings
 
