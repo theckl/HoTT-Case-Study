4d43d7b4223fa45e74ed50ded0a7a49537d8e907
theckl <thomas.eckl@liv.ac.uk>
2023-11-19 23:10:03 +0000

    Update inductive.lean

--- src/sets/inductive.lean
+++ src/sets/inductive.lean
@@ -13,14 +13,21 @@ def fin_dep_map_of_list (l : list (Σ (A : Type _), A)) :
   Π (m : fin_Set l.length), (list_nth_le l m.1 m.2).1 :=
 λ m, (list_nth_le l m.1 m.2).2
 
-/- An inductive structure on a set `A` consists of finitely many operations of arbitrary 
-   arity which satisfy an induction principle on `A`. -/
+/- An inductive structure on a type `A` consists of finitely many operations of arbitrary 
+   arity which satisfy an induction principle on `A` together with an eliminator. -/
 @[hott]
 class has_ind_structure (A : Type _) :=
-  (op : list (Σ (op_ar : ℕ), (fin_Set op_ar -> A) -> A))
+  (ops : list (Σ (op_ar : ℕ), (fin_Set op_ar -> A) -> A))
   (ind : Π {C : A -> Type _}, 
-           (Π (m : fin_Set op.length) (args : fin_Set (list_nth_le op m.1 m.2).1 -> A),
-              (Π k, C (args k)) -> C ((list_nth_le op m.1 m.2).2 args)) -> Π (a : A), C a)
+           (Π (m : fin_Set ops.length) (args : fin_Set (list_nth_le ops m.1 m.2).1 -> A),
+              (Π k, C (args k)) -> C ((list_nth_le ops m.1 m.2).2 args)) -> Π (a : A), C a)
+  (elim : Π {C : A -> Type _} 
+            (ih : Π (m : fin_Set ops.length) 
+                    (args : fin_Set (list_nth_le ops m.1 m.2).1 -> A),
+                    (Π k, C (args k)) -> C ((list_nth_le ops m.1 m.2).2 args)) 
+            (m : fin_Set ops.length) (args : fin_Set (list_nth_le ops m.1 m.2).1 -> A)
+            (f : Π k, C (args k)), 
+            @ind C ih ((list_nth_le ops m.1 m.2).2 args) = ih m args f)
 
 /- If the inductive construction contains no operation at all, or no constant (= operation
    with arity 0), `A` is empty. -/
@@ -34,9 +41,15 @@ begin
     { fapply ih ⟨0, nat.zero_lt_succ 1⟩ (λ arg, 0), intro k, 
       hinduction nat.not_lt_zero k.1 k.2 }, 
     { fapply ih ⟨1, nat.le_refl 2⟩ (fin_map_of_list [n]), intro k,
-      change Σ (m : ℕ), m < 1 at k, hinduction k,
-      have p : fst = 0, from nat.eq_zero_of_le_zero' (nat.le_of_succ_le_succ snd), 
-      sorry }
+      change Σ (m : ℕ), m < 1 at k, 
+      have p : k = ⟨0, nat.zero_lt_succ 0⟩, from 
+      begin
+        hinduction k, fapply sigma_eq, 
+          exact nat.eq_zero_of_le_zero' (nat.le_of_succ_le_succ snd), 
+          apply pathover_of_tr_eq, exact is_prop.elim _ _
+      end,
+      rwr p, exact ih_1 },
+   { intros C ih m args f, sorry }
 end  
 
 @[hott, instance]
